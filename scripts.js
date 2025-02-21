be70ba053ae48f8a82292df3713330f371f364ab
Chee Wan <cheer7w@gmail.com>
2025-02-21 11:27:42 +0800

    change:  取消首页JS滚动。

--- scripts.js
+++ scripts.js
@@ -43,20 +43,20 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// Banner Slider
-let slideIndex = 0;
-showSlides();
+// // Banner Slider
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-    let slides = document.querySelectorAll('.banner-content');
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
+// function showSlides() {
+//     let slides = document.querySelectorAll('.banner-content');
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";  
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}    
+//     slides[slideIndex-1].style.display = "block";  
+//     setTimeout(showSlides, 5000); // Change image every 5 seconds
+// }
 
 //在移动设备上，虚拟键盘的 "go" 按钮会触发表单提交。
 document.addEventListener('DOMContentLoaded', function() {
